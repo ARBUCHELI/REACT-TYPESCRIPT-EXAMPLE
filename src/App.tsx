d23f940ec4dc1f3aba528b8a39a70eee3b837dba
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-23 17:53:43 +0300

    functional/stateless component intro

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,15 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Message from './Message';
 
 class App extends Component {
-  render() {
-    //let firstValue: string = 'Manny';
-    //let firstValue: number = 34;
-    //let firstValue: boolean = true;
-    //let firstValue: number[] = [2, 3, 56];
-    //let firstValue: Array<string> = ['2', '3', 'manny'];
-
-    // tuple
-    let aTuple: [string, number] = ['Manny', 34];
-
-    // enum
-    enum Codes {first = 1, second = 2};
-
-    // any
-    //let firstName: any = 'Manny';
-    let firstName: any = 3;
-
-    // void
-    const warning = (): void => {
-      console.log('Warning');
-    }
-
+  render() { 
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            {/* The value {aTuple[0]} is of { typeof aTuple[0]} type! */}
-            The value {firstName} is of { typeof firstName} type!
-          </p>
+          <Message />
         </header>
       </div>
     );
