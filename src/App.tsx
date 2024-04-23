7dd38f25dd43d155851eda6699cd03f1270cf92d
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-23 17:41:28 +0300

    other complex types

--- src/App.tsx
+++ src/App.tsx
@@ -8,13 +8,30 @@ class App extends Component {
     //let firstValue: number = 34;
     //let firstValue: boolean = true;
     //let firstValue: number[] = [2, 3, 56];
-    let firstValue: Array<string> = ['2', '3', 'manny'];
+    //let firstValue: Array<string> = ['2', '3', 'manny'];
+
+    // tuple
+    let aTuple: [string, number] = ['Manny', 34];
+
+    // enum
+    enum Codes {first = 1, second = 2};
+
+    // any
+    //let firstName: any = 'Manny';
+    let firstName: any = 3;
+
+    // void
+    const warning = (): void => {
+      console.log('Warning');
+    }
+
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            The value {firstValue} is of { typeof firstValue} type!
+            {/* The value {aTuple[0]} is of { typeof aTuple[0]} type! */}
+            The value {firstName} is of { typeof firstName} type!
           </p>
         </header>
       </div>
