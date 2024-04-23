1f5f9903b18ad7aa0dbcccd974a7d8889aca9ebd
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-23 17:21:00 +0300

    basic types

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    //let firstValue: string = 'Manny';
+    //let firstValue: number = 34;
+    //let firstValue: boolean = true;
+    //let firstValue: number[] = [2, 3, 56];
+    let firstValue: Array<string> = ['2', '3', 'manny'];
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            The value {firstValue} is of { typeof firstValue} type!
+          </p>
+        </header>
+      </div>
+    );
+  }
+
 }
 
 export default App;
