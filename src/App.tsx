300748851e3b5889dc3ea59ef086e78fd0a6a2eb
ARBUCHELI <radio_seattle@hotmail.com>
2024-04-24 11:22:32 +0300

    class/stateful component syntax

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import logo from './logo.svg';
 import './App.css';
 import Message from './Message';
 
-class App extends Component {
+class App extends Component<any> {
+  componentWillMount() {
+    console.log('Almost there...');
+  }
+
+  componentDidMount() {
+    console.log('Finally...hello!');
+  }
+
   render() { 
     return (
       <div className="App">
